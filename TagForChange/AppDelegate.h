d5fba83c9da57fe4e6c8eedbd540589137dcd1cb
Natasha Murashev <nmurashev@gmail.com>
2013-12-07 03:57:30 -0800

    initial commit

---
+++ TagForChange/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TagForChange
+//
+//  Created by Natasha Murashev on 12/7/13.
+//  Copyright (c) 2013 NatashaTheRobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
